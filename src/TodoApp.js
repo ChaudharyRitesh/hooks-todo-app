2e0e808bc00fe4d2d1b2f2afe279613e9c792aee
ChaudharyRitesh <riteshchaudhary587@gmail.com>
2023-03-30 19:13:14 +0530

    hooks

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -15,16 +15,15 @@ function TodoApp() {
         padding: 0,
         margin: 0,
         height: "100vh",
-        backgroundColor: "#fafafa"
+        backgroundColor: "#fafafa",
       }}
-      elevation={0}
-    >
-      <AppBar color='primary' position='static' style={{ height: "64px" }}>
+      elevation={0}>
+      <AppBar color="primary" position="static" style={{ height: "64px" }}>
         <Toolbar>
-          <Typography color='inherit'>TODOS WITH HOOKS</Typography>
+          <Typography color="inherit">React Course App</Typography>
         </Toolbar>
       </AppBar>
-      <Grid container justify='center' style={{ marginTop: "1rem" }}>
+      <Grid container justify="center" style={{ marginTop: "1rem" }}>
         <Grid item xs={11} md={8} lg={4}>
           <TodosProvider>
             <TodoForm />
